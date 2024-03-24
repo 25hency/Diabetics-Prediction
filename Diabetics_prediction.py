11bd4f7c94a755fd714484d6abbc562d3b59b1d7
Hency <119163061+25hency@users.noreply.github.com>
2024-03-24 22:32:04 +0530

    Create Diabetics_prediction.py

---
+++ Diabetics_prediction.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+from mlxtend.plotting import plot_decision_regions
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.metrics import classification_report
+
+#reading the dataset
+diabetes_df = pd.read_csv('C:\\Users\Hency\\Downloads\\diabetes.csv')
+
+#counting the zeros with the NAN values
+diabetes_df_copy = diabetes_df.copy(deep = True)
+diabetes_df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = diabetes_df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+
+#inplace=True: data will be modified without returning a copy of the data or the original data
+diabetes_df_copy['Glucose'].fillna(diabetes_df_copy['Glucose'].mean(), inplace = True)
+diabetes_df_copy['BloodPressure'].fillna(diabetes_df_copy['BloodPressure'].mean(), inplace = True)
+diabetes_df_copy['SkinThickness'].fillna(diabetes_df_copy['SkinThickness'].median(), inplace = True)
+diabetes_df_copy['Insulin'].fillna(diabetes_df_copy['Insulin'].median(), inplace = True)
+diabetes_df_copy['BMI'].fillna(diabetes_df_copy['BMI'].median(), inplace = True)
+
+#standard scaling, help our ML model to give a better result
+#check
+sc_X = StandardScaler()
+X =  pd.DataFrame(sc_X.fit_transform(diabetes_df_copy.drop(["Outcome"],axis = 1),), columns=['Pregnancies',
+'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
+
+#Model Building: Splitting the dataset
+X = diabetes_df.drop('Outcome', axis=1)
+y = diabetes_df['Outcome']
+
+#split the data into training and testing data using the train_test_split function
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=7)
+
+#Building the model using RandomForest
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier(n_estimators=200)
+rfc.fit(X_train, y_train)
+
+#check the accuracy of the model on the training dataset
+rfc_train = rfc.predict(X_train)
+from sklearn import metrics
+print("Accuracy_Score =", format(metrics.accuracy_score(y_train, rfc_train)))
+
+#Getting the accuracy score for Random Forest
+from sklearn import metrics
+
+predictions = rfc.predict(X_test)
+print("Accuracy_Score =", format(metrics.accuracy_score(y_test, predictions)))
+
+#Building the model using DecisionTree
+from sklearn.tree import DecisionTreeClassifier
+
+dtree = DecisionTreeClassifier()
+dtree.fit(X_train, y_train)
+
+#making the predictions on the testing data directly as it is of more importance.
+#Getting the accuracy score for Decision Tree
+from sklearn import metrics
+
+predictions = dtree.predict(X_test)
+print("Accuracy Score =", format(metrics.accuracy_score(y_test,predictions)))
+
+#Classification report and confusion matrix of the decision tree model
+from sklearn.metrics import classification_report, confusion_matrix
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test,predictions))
+
+#shows that how much weightage each feature provides in the model building phase
+rfc.feature_importances_
+
+#Saving Model – Random Forest
+import pickle
+
+# Firstly we will be using the dump() function to save the model using pickle
+saved_model = pickle.dumps(rfc)
+
+# Then we will be loading that saved model
+rfc_from_pickle = pickle.loads(saved_model)
+
+# lastly, after loading that model we will use this to make predictions
+rfc_from_pickle.predict(X_test)
+
+# rfc.predict([[0,137,40,35,168,43.1,2.228,33]]) 5th patient
+
+if rfc.predict([[6, 148, 72, 35, 0, 33.6, 0.627, 50]]) == 1:
+  print("diabetic")
+else:
+  print("not diabetic")
